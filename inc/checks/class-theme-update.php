7b979f8430e9cdf3e90f09a95e43368022ad1dc2
Daniel Bachhuber <daniel@handbuilt.co>
2016-08-31 15:34:11 -0700

    Add a check for theme updates

---
+++ inc/checks/class-theme-update.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace runcommand\Doctor\Checks;
+
+use WP_CLI;
+
+/**
+ * Check whether WordPress themes are up to date
+ */
+class Theme_Update extends Check {
+
+	public static $when = 'after_wp_load';
+
+	public function run() {
+		ob_start();
+		WP_CLI::run_command( array( 'theme', 'list' ), array( 'format' => 'json' ) );
+		$ret = ob_get_clean();
+		$themes = ! empty( $ret ) ? json_decode( $ret, true ) : array();
+		$update_count = 0;
+		foreach( $themes as $theme ) {
+			if ( 'available' === $theme['update'] ) {
+				$update_count++;
+			}
+		}
+
+		if ( 1 === $update_count ) {
+			$this->status = 'warning';
+			$this->message = "1 theme has an update available.";
+		} else if ( $update_count ) {
+			$this->status = 'warning';
+			$this->message = "{$update_count} themes have updates available.";
+		} else {
+			$this->status = 'success';
+			$this->message = 'Themes are up to date.';
+		}
+
+	}
+
+}
