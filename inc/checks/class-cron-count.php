3d7b0a3139260e2f37777661a394272b0f71c147
Daniel Bachhuber <daniel@handbuilt.co>
2016-09-27 11:07:20 -0700

    Add checks for duplicate and excess crons

---
+++ inc/checks/class-cron-count.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace runcommand\Doctor\Checks;
+
+use WP_CLI;
+
+/**
+ * Check whether there is an excess total number of cron jobs.
+ */
+class Cron_Count extends Cron {
+
+	/**
+	 * Warn when there are greater than this number of cron jobs.
+	 *
+	 * @var integer
+	 */
+	protected $threshold_count = 50;
+
+	public function run() {
+		$crons = $this->get_crons();
+		$job_count = 0;
+		foreach( $crons as $timestamp => $jobs ) {
+			// 'cron' option includes a 'version' key... ?!?!
+			if ( 'version' === $timestamp ) {
+				continue;
+			}
+
+			foreach( $jobs as $job => $data ) {
+				$job_count++;
+			}
+		}
+
+		if ( $job_count >= $this->threshold_count ) {
+			$this->status = 'error';
+			$this->message = 'Total number of cron jobs exceeds expected threshold.';
+		} else {
+			$this->status = 'success';
+			$this->message = 'Total number of cron jobs is within normal operating expectations.';
+		}
+	}
+
+}
