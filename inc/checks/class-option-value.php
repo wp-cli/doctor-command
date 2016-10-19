7c6d6a23e3724faf0f323afa25a68fe64197da6c
Daniel Bachhuber <daniel@handbuilt.co>
2016-10-19 12:25:28 -0700

    Fix logic to check the correct variable

--- inc/checks/class-option-value.php
+++ inc/checks/class-option-value.php
@@ -27,19 +27,22 @@ class Option_Value extends Check {
 
 		$actual_value = get_option( $this->option );
 		if ( $actual_value == $this->expected_value ) {
-			$this->set_status( 'success' );
-			$this->set_message( "Option '{$this->option}' is '{$actual_value}' as expected." );
+			$status = 'success';
+			$message = "Option '{$this->option}' is '{$actual_value}' as expected.";
 		} else {
-			$this->set_status( 'error' );
-			$this->set_message( "Option '{$this->option}' is '{$actual_value}' but expected to be '{$this->expected_value}'." );
+			$status = 'error';
+			$message = "Option '{$this->option}' is '{$actual_value}' but expected to be '{$this->expected_value}'.";
 		}
+
+		$this->set_status( $status );
+		$this->set_message( $message );
 
 		// Message translation for options
 		switch ( $this->option ) {
 			case 'blog_public':
 				$public_actual = $actual_value ? 'public' : 'private';
 				$public_expected = 'public' === $public_actual ? 'private' : 'public';
-				if ( 'success' === $this->status ) {
+				if ( 'success' === $status ) {
 					$this->set_message( "Site is {$public_actual} as expected." );
 				} else {
 					$this->set_message( "Site is {$public_actual} but expected to be {$public_expected}." );
