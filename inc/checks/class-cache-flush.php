4ddbdf9002c0aa4d6a001b0dc6143ec82f235739
Sidsector9 <siddharth.thevaril@gmail.com>
2017-09-22 15:08:53 +0530

    GH#96 Replaced regex with Iterator

--- inc/checks/class-cache-flush.php
+++ inc/checks/class-cache-flush.php
@@ -3,23 +3,32 @@
 namespace runcommand\Doctor\Checks;
 
 use WP_CLI;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 
 /**
  * Detects the number of occurrences of the `wp_cache_flush()` function.
  */
-class Cache_Flush extends Check {
+class Cache_Flush extends File_Contents {
 
 	public function run() {
-		$result = exec( 'grep -r "wp_cache_flush()" ' . WP_CONTENT_DIR . ' | wc -l' );
+		$wp_content_dir = defined( 'WP_CONTENT_DIR' ) ? WP_CONTENT_DIR : ABSPATH . 'wp-content';
+		$directory = new RecursiveDirectoryIterator( $wp_content_dir, RecursiveDirectoryIterator::SKIP_DOTS );
+		$iterator = new RecursiveIteratorIterator( $directory, RecursiveIteratorIterator::CHILD_FIRST );
 
-		$plural = ( '1' === $result ) ? '' : 's';
+		$file_content = new File_Contents();
+		$file_content->regex = 'wp_cache_flush()';
 
-		if ( '0' === $result ) {
+		foreach ( $iterator as $file ) {
+			$file_content->check_file( $file );
+		}
+
+		if ( ! empty( $file_content->_matches ) ) {
 			$this->set_status( 'success' );
-			$this->set_message( '0 occurrences of wp_cache_flush() detected.' );
+			$this->set_message( 'Use of wp_cache_flush() detected.' );
 		} else {
 			$this->set_status( 'success' );
-			$this->set_message( $result . ' occurrence' . $plural . ' of wp_cache_flush() detected.' );
+			$this->set_message( 'Use of wp_cache_flush() not found.' );
 		}
 	}
 }
