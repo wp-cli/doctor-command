c2f1c4e5bef149b3793551cf72404f844b31da32
Sidsector9 <siddharth.thevaril@gmail.com>
2017-09-22 15:11:56 +0530

    GH#96 Improvised logic

--- inc/checks/class-cache-flush.php
+++ inc/checks/class-cache-flush.php
@@ -12,18 +12,20 @@ use RecursiveIteratorIterator;
 class Cache_Flush extends File_Contents {
 
 	public function run() {
+
+		// Path to wp-content directory.
 		$wp_content_dir = defined( 'WP_CONTENT_DIR' ) ? WP_CONTENT_DIR : ABSPATH . 'wp-content';
 		$directory = new RecursiveDirectoryIterator( $wp_content_dir, RecursiveDirectoryIterator::SKIP_DOTS );
 		$iterator = new RecursiveIteratorIterator( $directory, RecursiveIteratorIterator::CHILD_FIRST );
 
-		$file_content = new File_Contents();
-		$file_content->regex = 'wp_cache_flush()';
+		// Regex to match.
+		$this->regex = 'wp_cache_flush()';
 
 		foreach ( $iterator as $file ) {
-			$file_content->check_file( $file );
+			$this->check_file( $file );
 		}
 
-		if ( ! empty( $file_content->_matches ) ) {
+		if ( ! empty( $this->_matches ) ) {
 			$this->set_status( 'success' );
 			$this->set_message( 'Use of wp_cache_flush() detected.' );
 		} else {
