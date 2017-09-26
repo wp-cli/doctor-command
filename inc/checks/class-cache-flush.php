7c3110353c998838bc48ccaaa4642b7d8652462b
Sidsector9 <siddharth.thevaril@gmail.com>
2017-09-26 11:25:18 +0530

    GH#96 Updated with early return

--- inc/checks/class-cache-flush.php
+++ inc/checks/class-cache-flush.php
@@ -15,22 +15,23 @@ class Cache_Flush extends File_Contents {
 
 		// Path to wp-content directory.
 		$wp_content_dir = defined( 'WP_CONTENT_DIR' ) ? WP_CONTENT_DIR : ABSPATH . 'wp-content';
-		$directory = new RecursiveDirectoryIterator( $wp_content_dir, RecursiveDirectoryIterator::SKIP_DOTS );
-		$iterator = new RecursiveIteratorIterator( $directory, RecursiveIteratorIterator::CHILD_FIRST );
+		$directory      = new RecursiveDirectoryIterator( $wp_content_dir, RecursiveDirectoryIterator::SKIP_DOTS );
+		$iterator       = new RecursiveIteratorIterator( $directory, RecursiveIteratorIterator::CHILD_FIRST );
 
 		// Regex to match.
-		$this->regex = 'wp_cache_flush\(\)';
+		$this->regex    = 'wp_cache_flush\(\)';
 
 		foreach ( $iterator as $file ) {
 			$this->check_file( $file );
 		}
 
-		if ( ! empty( $this->_matches ) ) {
-			$this->set_status( 'warning' );
-			$this->set_message( 'Use of wp_cache_flush() detected.' );
-		} else {
+		if ( empty( $this->_matches ) ) {
 			$this->set_status( 'success' );
 			$this->set_message( 'Use of wp_cache_flush() not found.' );
+			return;
 		}
+
+		$this->set_status( 'warning' );
+		$this->set_message( 'Use of wp_cache_flush() detected.' );
 	}
 }
